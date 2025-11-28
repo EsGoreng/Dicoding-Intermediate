cfdf912e4e0f97828f0e14e2568ab6e9ac489ec3
EsGoreng <itsnaakhdan25@gmail.com>
2025-11-28 17:29:43 +0700

    retry

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,24 +1,7 @@
 export const ACCESS_TOKEN_KEY = 'accessToken';
 
-// Support both import.meta.env (Vite) dan process.env (Webpack)
-const getEnv = (key, defaultValue) => {
-  if (typeof import !== 'undefined' && import.meta?.env) {
-    return import.meta.env[key] || defaultValue;
-  }
-  return process.env[key] || defaultValue;
-};
+export const BASE_URL = process.env.VITE_API_BASE_URL || 'https://story-api.dicoding.dev/v1';
 
-export const BASE_URL = getEnv(
-  'VITE_API_BASE_URL',
-  'https://story-api.dicoding.dev/v1'
-);
+export const MAP_SERVICE_API_KEY = process.env.VITE_MAP_SERVICE_API_KEY || 'x02r9WSjHseBBp4yxRmy';
 
-export const MAP_SERVICE_API_KEY = getEnv(
-  'VITE_MAP_SERVICE_API_KEY',
-  'x02r9WSjHseBBp4yxRmy'
-);
-
-export const VAPID_PUBLIC_KEY = getEnv(
-  'VITE_VAPID_PUBLIC_KEY',
-  'BCCs2eonMI-6H2ctvFaWg-UYdDv387Vno_bzUzALpB442r2lCnsHmtrx8biyPi_E-1fSGABK_Qs_GlvPoJJqxbk'
-);
+export const VAPID_PUBLIC_KEY = process.env.VITE_VAPID_PUBLIC_KEY || 'BCCs2eonMI-6H2ctvFaWg-UYdDv387Vno_bzUzALpB442r2lCnsHmtrx8biyPi_E-1fSGABK_Qs_GlvPoJJqxbk';
--- webpack.common.js
+++ webpack.common.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
-require('dotenv').config();
 
 module.exports = {
   entry: {
